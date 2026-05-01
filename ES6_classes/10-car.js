caa6f31ff91f25cc3e63c3f667efd549ec455886
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-01 10:18:47 +0200

    Add Car class with cloning functionality

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,29 @@
+const clonedCar = Symbol('clonedCar');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this[Symbol.species]();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
