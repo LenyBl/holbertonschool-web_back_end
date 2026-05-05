9cb86ca89e8e79945dea8baae34b73c9341a276c
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-05 11:25:52 +0000

    refactor: update createInt8TypedArray function to use ArrayBuffer and DataView for setting values

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-	const arr = new Int8Array(length);
-	arr[position] = value;
-	return arr;
+	if (position >= length) {
+		throw new Error('Position outside range');
+	}
+	const buffer = new ArrayBuffer(length);
+	const view = new DataView(buffer);
+	view.setInt8(position, value);
+	return view;
 }
