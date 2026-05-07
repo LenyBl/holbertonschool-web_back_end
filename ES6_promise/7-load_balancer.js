468db0fca9ba05cb7b00739fc81a6d87ce98d273
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-07 08:14:45 +0000

    refactor: simplify loadBalancer function by removing logging and error handling

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,9 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-	  return Promise.race([chinaDownload, USDownload])
-	    .then((result) => {
-	      console.log(result);
-	    })
-	    .catch((error) => {
-	      console.error(error);
-	    });
+  return Promise.race([chinaDownload, USDownload]);
 }
