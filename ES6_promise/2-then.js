9a5e71f7eb08f0bbecd0fb1d4a72694d53d0d108
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-07 07:58:17 +0000

    fix: update handleResponseFromAPI to return status and body in response

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-	.then((response) => {
-	  console.log(response);
-	  return response;
-	})
-	.catch((error) => {
-	  console.log(error.message);
-	  return error;
-	});
+	return promise
+		.then((response) => {
+			console.log(response);
+			return response, { status: 200, body: 'Success' };
+		}
+		)
+		.catch((error) => {
+			console.log(error);
+			return new Error();
+		})
+		.finally(() => {
+			console.log('Got a response from the API');
+		});
 }
