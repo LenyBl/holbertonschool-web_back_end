c29ba4f6027772548279acbea5531310a8cbbe6e
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-07 08:19:54 +0000

    refactor: update guardrail function to handle arguments and return a queue of results

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,7 +1,11 @@
-export default function guardrail(mathFunction, ...args) {
+export default function guardrail(mathFunction) {
+  const queue = [];
   try {
-	return mathFunction(...args);
+    queue.push(mathFunction());
   } catch (error) {
-	return String(error);
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
   }
+  return queue;
 }
