45c0064991c967b8f8e05a666f955c2c5f5ce6c0
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-11 08:25:37 +0000

    refactor: format readline interface initialization for improved readability

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,10 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
