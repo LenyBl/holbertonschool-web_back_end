f285bb81909005d57b175950968b1c543ea884b5
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-12 07:31:47 +0000

    refactor: simplify HTTP server response handling and remove console log

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,10 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello Holberton School!');
-	  } else {
-	res.writeHead(404, { 'Content-Type': 'text/plain' });
-	res.end('Not Found');
-  }
+const app = http.createServer((_req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
