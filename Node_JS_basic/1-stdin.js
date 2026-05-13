b8691b57a869357176651bfdc33397e1cc0dd2a1
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2026-05-13 07:49:17 +0000

    fix: remove unnecessary terminal option from readline interface

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,6 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: false,
 });
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
